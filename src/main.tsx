f0616561113b2f41e1192f696f0a0de30614c35f
Adrien COUTURIER <adr.couturier@gmail.com>
2026-02-13 07:31:29 -0500

    fix : redirect url afte rlogin

--- src/main.tsx
+++ src/main.tsx
@@ -17,7 +17,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         domain={domain}
         clientId={clientId}
         authorizationParams={{
-          redirect_uri: window.location.origin,
+          redirect_uri: "https://divehub.info",
         }}
       >
         <App />
